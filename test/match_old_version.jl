e1cdb164e337ec1b94b10c5984d3d71d1c44a58e
Paul Grigas <paulgrigas@gmail.com>
2019-06-05 19:12:13 -0700

    add test

--- test/match_old_version.jl
+++ test/match_old_version.jl
@@ -1,4 +1,6 @@
-using DecisionTree
+using DecisionTree, Random, LinearAlgebra
+
+Random.seed!(352)
 
 n = 100
 p = 5
@@ -6,4 +8,10 @@ p = 5
 y = randn(n)
 X = randn(n, p)
 
+X_new = randn(2*n, p)
+
 model = build_forest(y, X, 2, 10)
+
+preds = apply_forest(model, X_new)
+
+norm(preds)
